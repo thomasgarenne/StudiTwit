5f58d7410b7dab6f25b4a97e2e5083e02c37c828
thomas <thomas.garenne@outlook.com>
2023-03-29 10:39:12 +0200

    Create User Form

---
+++ src/Controller/UserController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\LoginType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): Response
+    {
+        return $this->render('user/index.html.twig', [
+            'controller_name' => 'UserController',
+        ]);
+    }
+    #[Route('/user/new', name: 'app_user_new')]
+    public function new(UserPasswordHasherInterface $passwordHasher, Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User($passwordHasher);
+        $form = $this->createForm(LoginType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($user);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_post');
+        }
+
+        return $this->render('user/form.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
